245cf8bae9d953cf5c32a7d4617f596689a175a9
Akanni Silva <akanni029@gmail.com>
2026-03-30 20:50:17 -0300

    feat: registrar ApresentacaoController no ApresentacaoModule
    - ApresentacaoModule: adiciona ApresentacaoController aos controllers
    - Contexto: integrar controller REST ao módulo para exposição de endpoints

--- src/apresentacao/apresentacao.module.ts
+++ src/apresentacao/apresentacao.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Apresentacao } from './entities/apresentacao.entity';
 import { ApresentacaoService } from './services/apresentacao.service';
+import { ApresentacaoController } from './controllers/apresentacao.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Apresentacao])],
   providers: [ApresentacaoService],
-  controllers: [],
+  controllers: [ApresentacaoController],
   exports: [],
 })
 export class ApresentacaoModule {}
